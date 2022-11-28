197c973c4678a7c71440389d65fb28a55fa71711
abdul-rahman-1999 <rahmanabdul10@outlook.com>
2022-11-28 12:36:56 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useContext} from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Movies from './components/Movies';
+import Movie from './components/Movie';
+import Theater from './components/Theater';
+import Bookings from './components/Bookings';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Query from './components/Query';
+import Payment from './components/Payment';
+import Admin from './components/Admin';
+import AdminaddMovies from './components/AdminaddMovies';
+import AdmineditMovie from './components/AdmineditMovie';
+import SeatBooking from './components/SeatBooking';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <>
+  <Routes>
+  <Route path='/users/signup' element={<Signup/>}/>
+  <Route path='/users/login' element={<Login/>}/>
+  <Route path='/bookmyshow/movies/admin' element={<Admin/>}/>
+  <Route path='/bookmyshow/movies/add' element={<AdminaddMovies/>}/>
+  <Route path='/bookmyshow/movies/edit/:id' element={<AdmineditMovie/>}/>
+    <Route path='/bookmyshow/movies' element={<Movies/>}/>
+    <Route path='/bookmyshow/movies/query' element={<Query/>}/>
+    <Route path='/bookmyshow/movies/:id' element={<Movie/>}/>
+    <Route path='/bookmyshow/movies/:id/theater' element={<Theater/>}/>
+    <Route path='/bookmyshow/movies/:id/theater/:theaterId' element={<Bookings/>}/>
+    <Route path='/bookmyshow/movies/:id/theater/:theaterId/:showId/seatbooking' element={<SeatBooking/>}/>
+    <Route path='/bookmyshow/movies/:id/theater/:theaterId/:showId/seatbooking/:selected/:total' element={<Payment/>}/>
+  </Routes>
+  
+  </>
 }
 
 export default App;
